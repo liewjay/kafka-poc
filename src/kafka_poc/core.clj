89e3f741770ed93331f652a97987d139fe5874d5
Liew <P2715641@CORP.CHARTERCOM.COM>
2017-04-10 10:58:17 -0600

    Updating to work with kafka on DAAS

--- src/kafka_poc/core.clj
+++ src/kafka_poc/core.clj
@@ -1,25 +1,30 @@
-(ns kafka-poc.core)
+(ns kafka-poc.core
+  (:require [clojure.string :as str]))
 
 (defn consume-kafka
-  [group]
-  (let [props (doto (java.util.Properties.)
-                (.put "bootstrap.servers" "localhost:9092")
+  [group topic]
+  (let [deserializer (->
+                       org.apache.kafka.common.serialization.StringDeserializer
+                       .getName)
+        brokers ["aalx-ctec-a03p.enwd.co.sa.charterlab.com:9092"
+                 "aalx-ctec-a03p.enwd.co.sa.charterlab.com:9093"
+                 "aalx-ctec-a03p.enwd.co.sa.charterlab.com:9094"
+                 "aalx-ctec-a03p.enwd.co.sa.charterlab.com:9095"]
+        props (doto (java.util.Properties.)
+                (.put "bootstrap.servers" (str/join "," brokers))
                 (.put "group.id" group)
-                (.put "key.deserializer" (->
-                                           org.apache.kafka.common.serialization.StringDeserializer
-                                           .getName))
-                (.put "value.deserializer" (->
-                                             org.apache.kafka.common.serialization.StringDeserializer
-                                             .getName)))
+                (.put "key.deserializer" deserializer)
+                (.put "value.deserializer" deserializer))
         consumer (org.apache.kafka.clients.consumer.KafkaConsumer. props)
-        _ (.subscribe consumer ["newt"])
+        _ (.subscribe consumer [topic])
         records (->
                   (.poll consumer 1000)
                   .iterator
                   iterator-seq)
+        _ (println records (count records))
         _ (.close consumer)]
     (map (fn [record]
            {:offset (.offset record)
             :value (.value record)}) records)))
 
-(consume-kafka "jaytest")
+(consume-kafka "jaytest" "newt")
